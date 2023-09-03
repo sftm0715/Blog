80d472f2c7cdce86eb72c282352c9e4fc4069222
sftm0715 <sftm0715@gmail.com>
2023-09-03 23:59:31 +0900

    feat : 회원 도메인

---
+++ src/main/java/me/jeongdahee/springbootdeveloper/domain/User.java
@@ -0,0 +1,76 @@
+package me.jeongdahee.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails { // UserDetails 를 상속 방아 인증 객체로 사용
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    @Override // 권환 반환
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override // 사용자의 id 반환 (고유값) :
+    public String getUsername() {
+        return email;
+    }
+
+    @Override // 사용자 패스워드 반환
+    public String getPassword() {
+        return password;
+    }
+
+    @Override // 계정 만료 여부 반환
+    public boolean isAccountNonExpired() {
+        // 만료됐는지 확인하는 로직
+        return true; // true : 잠금 X
+    }
+
+    @Override // 계정 잠금 여부 반환
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override // 패스워드 만료 여부 반환
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override // 계정 사용 가능 여부 반환
+    public boolean isEnabled() {
+        // 계정이 사용 가능한지 확인하는 로직
+        return true;
+    }
+
+}
